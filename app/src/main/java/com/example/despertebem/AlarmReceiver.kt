e3bbf0c15f83c120ad1bf092802ae57c2700ca0f
VictoriaMollica <73254429+VictoriaMollica@users.noreply.github.com>
2026-05-17 19:57:00 -0300

    Update AlarmReceiver.kt
    Adição de comentários para entendimento de futuros desenvolvimentos

--- app/src/main/java/com/example/despertebem/AlarmReceiver.kt
+++ app/src/main/java/com/example/despertebem/AlarmReceiver.kt
@@ -8,24 +8,30 @@ import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 
+//Cria a notificação do alarme e inicia o serviço foreground que toca o som gradualmente
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
 
+        //ID unico do canal de notificação
         val channelId = "alarm_channel"
 
+        //ontem o serviço responsavel por gerenciar notificações do sistema
         val notificationManager =
             context.getSystemService(Context.NOTIFICATION_SERVICE)
                     as NotificationManager
 
+        //Cria o canal de notificação
         val channel = NotificationChannel(
             channelId,
             "Alarm Notifications",
             NotificationManager.IMPORTANCE_HIGH
         )
 
+        //Registra o canal no sistema e caso já exista, será reutilizado
         notificationManager.createNotificationChannel(channel)
 
+        //Monta a notificação que será exibida ao usuario quando o alarme for acionado
         val notification = NotificationCompat.Builder(context, channelId)
             .setContentTitle("Desperte Bem")
             .setContentText("Hora de acordar!")
@@ -33,6 +39,7 @@ class AlarmReceiver : BroadcastReceiver() {
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .build()
 
+        //Exibe a notificação
         notificationManager.notify(1, notification)
 
         // START GRADUAL ALARM SERVICE
